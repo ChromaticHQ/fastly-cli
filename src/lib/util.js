b9517590bad324fcb76f62c7f0b56f603ca9eb58
Alfonso Gómez-Arzola <alfonso@chromatichq.com>
2017-05-27 11:20:01 -0400

    Extract common functions into util library.

---
+++ src/lib/util.js
@@ -0,0 +1,53 @@
+const chalk = require(`chalk`);
+
+const apiKeyDescription = `Fastly API Key`;
+const apiKeyErrorMessage = `[error] ${apiKeyDescription} is required.`;
+const apiKeyOption = `-k, --apikey <required>`;
+const serviceIdDescription = `Fastly Service ID`;
+const serviceIdErrorMessage = `[error] ${serviceIdDescription} is required.`;
+const serviceIdOption = `-s, --serviceid <required>`;
+const hardPurgeOption = `-h, --hardpurge`;
+const hardPurgeDescription = `Hard purge immediately; do not use soft purge option.`;
+
+module.exports = {
+  apiKeyPresent (program) {
+    if (!program.apikey) {
+      console.error(chalk.red(apiKeyErrorMessage));
+      return false;
+    }
+    return true;
+  },
+
+  serviceIdPresent (program) {
+    if (!program.serviceid) {
+      console.error(chalk.red(serviceIdErrorMessage));
+      return false;
+    }
+    return true;
+  },
+
+  ResponseHandler (successMessage) {
+    return (err, fastlyResponseBody) => {
+      if (err) {
+        return console.dir(err);
+      }
+
+      const prettyjson = require(`prettyjson`);
+      const options = {};
+      console.log(prettyjson.render(fastlyResponseBody, options));
+      if (successMessage) {
+        console.log(chalk.green(successMessage));
+      }
+      process.stdin.pause();
+    };
+  },
+
+  apiKeyDescription,
+  apiKeyOption,
+  serviceIdDescription,
+  serviceIdOption,
+  apiKeyErrorMessage,
+  serviceIdErrorMessage,
+  hardPurgeOption,
+  hardPurgeDescription,
+};
